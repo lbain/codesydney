dbda3d4173b00e24c03e496d442f94641e610ad7
Lucy Bain <lucybain@gmail.com>
2014-05-20 17:41:31 +1000

    change url structure for updates

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "updates"
 
-  blog.permalink = "{year}/{title}.html"
+  blog.permalink = "{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
