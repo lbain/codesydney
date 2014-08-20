a8cd8c2e5dbc5158063190daf7ff844cfe93b14a
Lucy Bain <lucybain@gmail.com>
2014-08-20 19:59:43 +1000

    And build

--- build/javascripts/build-haml.js
+++ build/javascripts/build-haml.js
@@ -1,6 +1,1 @@
-$.each($('.lesson a'), function( index, link ) {
-  var title = $.trim($(link).find('.lesson-title h3').text());
-  var url = 'http://www.theodinproject.com' + $(link).attr('href');
-  var result = "%li #{link_to('" + title + "', '" + url + "')}";
-  console.log(result);
-});
+$.each($(".lesson a"),function(t,o){var e=$.trim($(o).find(".lesson-title h3").text()),n="http://www.theodinproject.com"+$(o).attr("href"),i="%li #{link_to('"+e+"', '"+n+"')}";console.log(i)});
